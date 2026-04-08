3495969561b7c7c433f18f977f566a5ba1aa1740
illiakovalenko <illia.kovalenko@sitecore.com>
2026-04-08 14:57:14 +0300

    Added more logs

--- proxy.ts
+++ proxy.ts
@@ -4,13 +4,26 @@ export default function proxy(
   request: NextRequest,
   fetchEvent: NextFetchEvent
 ) {
-    const startTime = Date.now();
+  const startTime = Date.now();
   const { pathname } = request.nextUrl;
   const { searchParams } = request.nextUrl;
   const { headers } = request;
   const { body } = request;
   const { method } = request;
   const { url } = request;
+
+  console.log('Visitor:', {
+    pathname,
+    searchParams,
+    headers,
+    body,
+    method,
+    url,
+    userAgent: headers.get('user-agent'),
+    referer: headers.get('referer'),
+    host: headers.get('host'),
+    
+  })
 
   console.log(pathname, searchParams, headers, body, method, url);
   console.log("Fetch Event:", fetchEvent);
